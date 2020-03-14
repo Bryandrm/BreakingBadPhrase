3825cf8849d13fe76778a7038eb752260c9c3da5
bryan <bryandrm97@gmail.com>
2020-03-13 18:18:18 -0600

    breaking bad project, fetch function has been created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+
+const Container = styled.div`
+  display: flex
+  align-items: center;
+  paddding-top: 5rem;
+  flex-direction: column;
+
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  const RequestAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const phrase = await api.json();
+    console.log(phrase[0]);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Container> 
+      <Boton
+        onClick={RequestAPI}
+      >
+        Get Quote
+      </Boton>
+    </Container>
   );
 }
 
